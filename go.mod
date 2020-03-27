f486531be87c4bf8543e4a1f31cf75797dc6fa7c
Katyal <rohkat@amazon.com>
2020-03-27 03:49:07 -0700

    add docker compose

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/go-redis/redis/v7 v7.2.0 // indirect
 	github.com/google/uuid v1.1.1
 )
--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 import (
 	"net/http"
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
+    "github.com/go-redis/redis"
+    "fmt"
 )
 
 func main() {
 	r := gin.Default()
+    client := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+		Password: "",
+		DB: 0,
+	})
+
+    pong, err := client.Ping().Result()
+    fmt.Println(pong, err)
+    
+    err = client.Set("hit",1, 0).Err()
+    if err != nil {
+        fmt.Println(err)
+    }
+
 
 	r.GET("/", func(c *gin.Context) {
+    
 		c.String(http.StatusOK, "OK")
     })
     
     r.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello I am the new Api")
+        val, err := client.Incr("hit").Result()
+
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        c.String(http.StatusOK, "The page has been visited for %d",val)
+
     })
 	r.Run(":8080")
+}
+
+func getcounter() {
+    
 }
